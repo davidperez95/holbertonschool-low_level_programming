3f938041fe77bb022f48ed8fc70ca24a9f111c2e
David Perez <daperezcruz@gmail.com>
2022-06-15 13:17:40 -0500

    task 2 fix 4

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,13 @@ void print_alphabet_x10(void)
 	char str = 97;
 	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		if (i != 9)
-			_putchar('\n');
-		else
-			continue;
 
 		for (str = 97; str <= 122; str++)
+		{
 			_putchar(str);
+			_putchar('\n');
+		}
 	}
 }
