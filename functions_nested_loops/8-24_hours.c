eb678ef0b827e7202f29e81f1cdc29533e6af093
David Perez <daperezcruz@gmail.com>
2022-06-15 21:21:45 -0500

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hours, minutes, i;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		if (hours < 10)
+			_putchar('0' + hours);
+		else
+			_putchar(hours);
+
+		_putchar(':');
+
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			if (minutes < 10)
+				_putchar('0' + minutes);
+			else
+				_putchar(minutes);
+		}
+	}
+}
