3faf81a618b2f893433f66ca2ab0f428051c3c3f
David Perez <daperezcruz@gmail.com>
2022-06-22 10:02:01 -0500

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: integer one
+ * @b: integer two
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int altern = a;
+
+	a = b;
+	altern = b;
+}
