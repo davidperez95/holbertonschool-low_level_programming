9123e18534bccc8a1e243a3553eaea31a5e395b1
David Perez <daperezcruz@gmail.com>
2022-06-27 16:54:02 -0500

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,4 +9,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int len = 0, len_2 = 0;
 
+	while (dest[len] != '\0')
+		len++;
+
+	for (len_2 = 0; len_2 >= 0; )
+	{
+		dest[len] = src[len_2];
+		if (src[len_2] == '\0')
+			break;
+		len++;
+		len_2++;
+	}
+
+	return (dest);
+}
