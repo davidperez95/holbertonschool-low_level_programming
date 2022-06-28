0dbbe6ade40950fd4b7ee2f7a4412b6dbf03520f
David Perez <daperezcruz@gmail.com>
2022-06-28 16:13:23 -0500

    task 1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to s, to fill with memory
+ * @b: byte to fill with
+ * @n: number of bytes to fill in s
+ *
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
