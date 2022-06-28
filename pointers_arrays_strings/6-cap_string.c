d23ca7f1bf840b348cecf92fa52eb3c647525f37
David Perez <daperezcruz@gmail.com>
2022-06-27 23:24:41 -0500

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string to capitalize
+ *
+ * Return: pointer to str
+ */
+char *cap_string(char *str)
+{
+	int i = 0, j = 0;
+	char separatos[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == separatos[j])
+			{
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+					str[i + 1] -= 32;
+				break;
+			}
+		}
+	}
+	return (str);
+}
