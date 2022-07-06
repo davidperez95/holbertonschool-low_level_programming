7d60cb83f267013a42fae695e8145bb4b6c2b48a
David Perez <daperezcruz@gmail.com>
2022-07-05 21:24:08 -0500

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers.
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i = 0, mult = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			mult *= atoi(argv[i]);
+
+		printf("%d\n", mult);
+		return (EXIT_SUCCESS);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
+
+
