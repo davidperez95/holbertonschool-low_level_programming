0611dcc4f7dd07a3092c98f19581ee6457833e1b
David Perez <daperezcruz@gmail.com>
2022-07-05 20:56:23 -0500

    argc and argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints its name
+ *
+ * Return: 0
+ *
+ */
+int main(int argc __attribute__((__unused__)), char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (EXIT_SUCCESS);
+}
