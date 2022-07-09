a399c2ebe89f597394744e630577e8cc9899c89e
David Perez <daperezcruz@gmail.com>
2022-07-08 23:04:58 -0500

    task 1 fix 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,15 +30,15 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_string = NULL;
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j = 0, len_s1 = _strlen(s1), len_s2 = _strlen(s2);
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= _strlen(s2))
+	if (n >= len_s2)
 	{
-		new_string = malloc(sizeof(*new_string) * (_strlen(s1) + _strlen(s2) + 1));
+		new_string = malloc(sizeof(*new_string) * (len_s1 + len_s2 + 1));
 		if (!new_string)
 		{
 			free(new_string);
@@ -52,9 +52,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		new_string[i] = '\0';
 		return (new_string);
 	}
-	if (n < _strlen(s2))
+	if (n < len_s2)
 	{
-		new_string = malloc(sizeof(*new_string) * (_strlen(s1) + n + 1));
+		new_string = malloc(sizeof(*new_string) * (len_s1 + n + 1));
 		if (!new_string)
 		{
 			free(new_string);
