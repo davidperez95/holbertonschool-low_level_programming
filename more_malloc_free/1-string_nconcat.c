ccf08b764139ea8de5506ab3f01a99b48bd6080a
David Perez <daperezcruz@gmail.com>
2022-07-09 00:04:25 -0500

    task 1 improvement

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -42,13 +42,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len_s2 = _strlen(s2);
 
 	if (n >= len_s2)
-	{
 		n = len_s2;
-		new_string = malloc(sizeof(*new_string) * (len_s1 + len_s2 + 1));
-	}
 
-	if (n < len_s2)
-		new_string = malloc(sizeof(*new_string) * (len_s1 + n + 1));
+	new_string = malloc(sizeof(*new_string) * (len_s1 + n + 1));
 
 	if (!new_string)
 	{
