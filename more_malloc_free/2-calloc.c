e5d06a5adb640e74c755193f7c59088989e0dfbc
David Perez <daperezcruz@gmail.com>
2022-07-09 14:39:58 -0500

    task 2 fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(*array) * nmemb);
+	array = malloc(sizeof(size) * nmemb);
 
 	if (!array)
 	{
