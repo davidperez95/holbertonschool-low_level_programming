a893e759c33be4cc54dccee04357f80192477d3a
David Perez <daperezcruz@gmail.com>
2022-07-13 16:48:00 -0500

    structures and typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struc dog - dog struct
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ *
+ * Description: creates a new dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* _DOG_ */
