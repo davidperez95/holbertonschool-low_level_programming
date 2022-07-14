83ef6da7b6bc6c3da402c377f57f0d1f9311eb65
David Perez <daperezcruz@gmail.com>
2022-07-13 19:17:35 -0500

    task 4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,19 +11,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *new_name = name;
-	char *new_owner = owner;
+	int i = 0, len_name = 0, len_owner = 0;
 
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (new_dog == NULL || !name || !owner)
 	{
 		free (new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = new_name;
+	for (len_name = 0; name[len_name]; )
+		len_name++;
+
+	for (len_owner = 0; owner[len_owner]; )
+		len_owner++;
+
+	new_dog->name = malloc(len_name + 1);
+	new_dog->owner = malloc(len_owner + 1);
+
+	if (!new_dog->name || !new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < len_name; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+
 	new_dog->age = age;
-	new_dog->owner = new_owner;
+
+	for (i = 0; i < len_owner; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner = '\0';
 
 	return (new_dog);
 }
