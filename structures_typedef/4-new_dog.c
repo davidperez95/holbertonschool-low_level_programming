a9b0bcf38ea62a12a72990cc04fe3b0dc7936f25
David Perez <daperezcruz@gmail.com>
2022-07-13 19:21:59 -0500

    task 4 fix 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	int i = 0, len_name = 0, len_owner = 0;
 
-	new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(*new_dog));
 	if (new_dog == NULL || !(name) || !(owner))
 	{
 		free(new_dog);
