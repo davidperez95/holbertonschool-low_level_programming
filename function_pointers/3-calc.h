c8fa357af2f098972496ecf0f3e0768db0138820
David Perez <daperezcruz@gmail.com>
2022-07-15 18:53:07 -0500

    variadic functions

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,9 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/* importing */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* types and structures */
 
 /**
  ** struct op - Struct op
@@ -17,11 +20,17 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+/* macros valores constantes */
+
+/* prototypes */ 
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+/* macros functions */
 
 #endif /* _MAIN_H_ */
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,25 @@
+#ifndef _MAIH_H_
+#define _MAIN_H_
+
+/* importing */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+/* types and structures */
+
+
+/* macros constant values */ 
+
+
+/* prototypes */
+
+int sum_them_all(const unsigned int n, ...);
+
+
+/* macro functions */ 
+
+
+#endif /* _MAIN_H_ */
