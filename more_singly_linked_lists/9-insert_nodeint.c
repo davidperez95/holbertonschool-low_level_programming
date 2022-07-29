2613ea34e7d654c2154fedd01085783dd70e36b0
David Perez <daperezcruz@gmail.com>
2022-07-29 13:49:49 -0500

    task 9 case fix 2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,42 +10,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0, list_len = 0;
+	unsigned int i = 0;
 	listint_t *new_node = NULL, *temp = NULL;
 
 	if (head == NULL)
 		return (NULL);
 
 	temp = *head;
-	for (list_len = 0; *head; list_len++)
-		temp = temp->next;
+
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
+			h = h->next;
+	}
+
+	if (temp == NULL && idx != 0)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+	new_node->n = n;
 
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		new_node->n = n;
 		*head = new_node;
-		return (new_node);
+	}
+	else
+	{
+		new_node->next = temp->next;
+		temp->next = new_node;
 	}
 
-	temp = *head;
-	if (idx > list_len)
-		return (NULL);
-	for (i = 1; i < idx; i++)
-		temp = temp->next;
-
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
 	return (new_node);
 }
