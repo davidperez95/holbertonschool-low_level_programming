c2da6505d7956687e974dbefd9284aa564715d25
David Perez <daperezcruz@gmail.com>
2022-07-29 13:13:43 -0500

    task 9 betty fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 
-	if (*head == NULL);
+	if (*head == NULL)
 	{
 		*head = new_node;
 		new_node->next = NULL;
@@ -43,5 +43,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = temp->next;
 	temp->next = new_node;
-	return (new_node);		       
+	return (new_node);
 }
