3e84beaa0e9b1fa98c528168f3c88f73e27c4278
David Perez <daperezcruz@gmail.com>
2022-07-28 20:07:41 -0500

    more singly linkded list

---
+++ more_singly_linked_lists/lists.h
@@ -0,0 +1,35 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+/* importing */
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/* types and structures */
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/*macros constatn values */
+
+/* prototypes */
+
+size_t print_listint(const listint_t *h);
+
+/* macro functions */
+
+#endif /*_LISTS_H_ */
