bfa1f1958a2f196e47111af16d880fccf0fea795
David Perez <daperezcruz@gmail.com>
2022-08-01 23:03:29 -0500

    Bit manipulation

---
+++ bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/* importing */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* types and structures */
+
+/* macros constant values */
+
+/* prototipos */
+
+unsigned int binary_to_uint(const char *b);
+
+/* macro functions */
+
+#endif /* _MAIN_H_ */
---
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the head of the list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+	const listint_t *one = head, two = head;
+
+	if (head == NULL)
+		exit(98);
+
+	while (one && two && two->next && head)
+	{
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
+	head = NULL;
+	return (nodes);
+}
