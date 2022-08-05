0211f23531634c8d4c72086dbf01cca41e87347f
David Perez <daperezcruz@gmail.com>
2022-08-04 22:45:45 -0500

    task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,5 +8,28 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
+	dlistint_t *new_node = NULL, *temp = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		temp = *head;
+		temp->prev = new_node;
+		*head = new_node;
+		new_node->next = temp;
+	}
+
+	return (new_node);	
 }
