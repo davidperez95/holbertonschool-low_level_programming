27f303179d4bf91de43892677621a00f6da19470
David Perez <daperezcruz@gmail.com>
2022-08-30 14:48:32 -0500

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	int i = 0;
+	unsigned long int i = 0;
 	hash_table_t *new_table = NULL;
 
 	new_table = malloc(sizeof(hash_table_t));
