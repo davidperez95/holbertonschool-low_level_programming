f0d1edd71704b8425ea4ad03caa5fc86a78e0091
David Perez <daperezcruz@gmail.com>
2022-09-12 12:35:57 -0500

    search algorithms

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: array to search on
+ * @size: size of the array
+ * @value: value to search for
+ * Return: the first index where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = 0;
+
+	if (!array)
+		return (-1);
+
+	for (; index < size; index++)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return(index);
+	}
+
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_H_
+#define _SEACH_H_
+
+/* Importing */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_H_ */
